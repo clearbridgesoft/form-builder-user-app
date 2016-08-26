3d141b2bd7579804df4797e3f1862d11840cdafd
link <esurllink@gmail.com>
2016-08-26 10:29:36 +1000

    styling

--- client/main.js
+++ client/main.js
@@ -16,71 +16,97 @@ import './main.html';
 
 
 
+
+
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      db: {
+        demoForm:{
+          schema:{
+            "title": "A registration form",
+            "description": "A simple form example.",
+            "type": "object",
+            "properties": {
+              "firstName": {
+                "type": "string",
+                "title": "First name"
+              },
+              "lastName": {
+                "type": "string",
+                "title": "Last name"
+              },
+              "age": {
+                "type": "integer",
+                "title": "Age"
+              },
+              "bio": {
+                "type": "string",
+                "title": "Bio"
+              },
+              "password": {
+                "type": "string",
+                "title": "Password",
+                "minLength": 3
+              }
+            }
+          },
+          uiSchema:{
+            "age": {
+              "ui:widget": "updown"
+            },
+            "bio": {
+              "ui:widget": "textarea"
+            },
+            "password": {
+              "ui:widget": "password",
+              "ui:help": "Hint: Make it strong!"
+            },
+            "date": {
+              "ui:widget": "alt-datetime"
+            }
+          }
+        }
+      }
+    };
   }
   onSubmit(formSchema){
+    this.state.db[formSchema.name]={
+      schema:formSchema.schema,
+      uiSchema:formSchema.uiSchema,
+    };
     console.log(formSchema);
+    //this.setState(this.state);
+    this.setState({editing:undefined});
+  }
+  listForms(){
+    return (<ul>
+      {Object.keys(this.state.db).map((formName)=>(
+        <li key={formName}><span>{formName}</span> <button onClick={this.onEdit.bind(this,formName)}>edit</button> </li>
+      ))}
+      </ul>);
+  }
+  onEdit(formName){
+    console.log(formName);
+    this.setState({editing:formName});
   }
   render(){
+    if(this.state.editing){
+      const schema = this.state.db[this.state.editing];
+      return this.buildForm(this.state.editing,schema);
+    }else{
+      return this.listForms();
+    }
+  }
+  buildForm(formName,formSchema){
     return (<FormBuilder
-                preset={preset}
-                fields={customized_widgets}
-                onSubmit={this.onSubmit}
-                formName={"developing"}
-                formSchema={{
-                    schema:{
-                      "title": "A registration form",
-                      "description": "A simple form example.",
-                      "type": "object",
-                      "required": [
-                        "firstName",
-                        "lastName"
-                      ],
-                      "properties": {
-                        "firstName": {
-                          "type": "string",
-                          "title": "First name"
-                        },
-                        "lastName": {
-                          "type": "string",
-                          "title": "Last name"
-                        },
-                        "age": {
-                          "type": "integer",
-                          "title": "Age"
-                        },
-                        "bio": {
-                          "type": "string",
-                          "title": "Bio"
-                        },
-                        "password": {
-                          "type": "string",
-                          "title": "Password",
-                          "minLength": 3
-                        }
-                      }
-                    },
-                    uiSchema:{
-                      "age": {
-                        "ui:widget": "updown"
-                      },
-                      "bio": {
-                        "ui:widget": "textarea"
-                      },
-                      "password": {
-                        "ui:widget": "password",
-                        "ui:help": "Hint: Make it strong!"
-                      },
-                      "date": {
-                        "ui:widget": "alt-datetime"
-                      }
-                    }
-                  }}
-
-
-            />);
+      preset={preset}
+      fields={customized_widgets}
+      onSubmit={this.onSubmit.bind(this)}
+      formName={this.state.editing}
+      formSchema={this.state.db[this.state.editing]}
+      />);
   }
 }
 
